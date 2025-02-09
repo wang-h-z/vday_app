b3f439e728108a441e56203dc32e65a9c759dac9
haozhen <whaozhen01@gmail.com>
2025-02-09 08:26:11 +0800

    Bug fix attempt for npm run dev

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
+import tailwindcss from 'tailwindcss';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
