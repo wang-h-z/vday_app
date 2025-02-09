746e98fccd8bfc1303f07432e33640c27c35cfe4
haozhen <whaozhen01@gmail.com>
2025-02-09 09:21:16 +0800

    Deployment fix

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const baseUrl = import.meta.env.PROD 
+export const redirectUri = import.meta.env.PROD 
   ? 'https://vday-app-phi.vercel.app'  // Your Vercel deployment URL
   : 'http://localhost:5173';  // Local development URL
 export const scopes = [
